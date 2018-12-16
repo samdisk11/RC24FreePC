bd5000fde646fab297208834ee5be916ad775a7d
samdisk11 <thexkey38@gmail.com>
2018-12-16 11:32:36 -0800

    Add RuleSystem program
    rule system for VPS's that should be placed in startup and be readonly

---
+++ RuleSystem/Source Code [C#]/RuleSystem/RuleSystem/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Hi there, welcome to the Source Code of RuleSystem!
+// first lets define some really cool stuff
+//public String Rules = Application.StartupPath + "\\rules.txt";
+namespace RuleSystem
+{
+    public partial class Form1 : Form
+    {
+        public String Rules = Application.StartupPath + "\\rules.txt";
+        public Form1()
+        {
+            InitializeComponent();
+           
+            
+            string content = File.ReadAllText(Rules);
+            richTextBox1.Text = content;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
